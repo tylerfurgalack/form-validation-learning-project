579b881e93afdd3a1c7184c609bc3a6d1fca38e7
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-08-09 20:35:25 -0400

    UI and Bootstrap

---
+++ app.js
@@ -0,0 +1,22 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+  const name = document.getElementById('name');
+  const re = //;
+}
+
+function validateZip() {
+  
+}
+
+function validateEmail() {
+  
+}
+
+function validatePhone() {
+  
+}
